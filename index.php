aec941bcc2aa8e4618323d12e0e78a50c52efd5e
fortunato <sambataro1990@gmail.com>
2021-01-22 10:00:44 +0100

    migliorato

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 $faqs = [
   [
-    "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?"
-    =>
+    "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+    "answer" =>
     "La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
     <br>
     <br>
@@ -17,8 +17,8 @@ $faqs = [
   ],
 
   [
-    "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?"
-    =>
+    "question" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+    "answer" =>
     "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
     <br>
     <br>
@@ -31,13 +31,13 @@ $faqs = [
     Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
   ],
   [
-    "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?"
-    =>
+    "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+    "answer" =>
     "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui.</a> Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a>",
   ],
   [
-    "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?"
-    =>
+    "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+    "answer" =>
     "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL</a> referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
   ],
 ];
@@ -58,10 +58,8 @@ $faqs = [
     <main>
       <?php foreach ($faqs as $faq ) { ?>
         <div>
-          <?php foreach ($faq as $question => $answer) {?>
-            <h2><?php echo $question; ?></h2>
-            <p><?php echo $answer; ?></p>
-          <?php } ?>
+            <h2><?php echo $faq["question"]; ?></h2>
+            <p><?php echo $faq["answer"]; ?></p>
         </div>
       <?php } ?>
     </main>
